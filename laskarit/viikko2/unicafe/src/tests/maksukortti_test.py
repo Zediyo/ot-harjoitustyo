2385e23cb54fff2842ebb1394e1b824b575aee4a
Zedii <47713084+Zediyo@users.noreply.github.com>
2025-03-21 14:24:04 +0200

    teht6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -2,8 +2,30 @@ import unittest
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
-    def setUp(self):
-        self.maksukortti = Maksukortti(1000)
+	def setUp(self):
+		self.maksukortti = Maksukortti(1000)
 
-    def test_luotu_kortti_on_olemassa(self):
-        self.assertNotEqual(self.maksukortti, None)
+	def test_luotu_kortti_on_olemassa(self):
+		self.assertNotEqual(self.maksukortti, None)
+        
+	### t6 ###
+	def test_saldo_alussa_oikein(self):
+		self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+	def test_saldo_lataus(self):
+		self.maksukortti.lataa_rahaa(500)
+		self.assertEqual(self.maksukortti.saldo_euroina(), 15.0)
+
+	def test_saldo_otto(self):
+		self.maksukortti.ota_rahaa(500)
+		self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+	def test_saldo_otto_ylitys(self):
+		self.maksukortti.ota_rahaa(1100)
+		self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+	def test_saldo_otto_true(self):
+		self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+	def test_saldo_otto_false(self):
+		self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
